0dac586dc8942556d1a35798e40f1e2cd7578871
Cyber-Strife <hogoruf185@gmail.com>
2025-02-18 22:55:31 +0900

    Create App.js

---
+++ src/web/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Cyber Strife!</h1>
+      <p>Prepare your AI Overlord for the battle ahead!</p>
+    </div>
+  );
+}
+
+export default App;
