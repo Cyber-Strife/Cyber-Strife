af8b2b954ffeb01d9f5b7785a2f51eb2d47bcccc
Cyber-Strife <hogoruf185@gmail.com>
2025-02-18 22:58:01 +0900

    Create models.py

---
+++ src/web/backend/database/models.py
@@ -0,0 +1,13 @@
+# models.py
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    score = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return f"<Player {self.name}>"
